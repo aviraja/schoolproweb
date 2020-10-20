544caa872800563ab5558c671d9d3e2720902ed0
unknown <avinashjadhav@hotmail.com>
2020-10-21 00:26:39 +0530

    firebase

---
+++ init-firebase.js
@@ -0,0 +1,13 @@
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD47XEheWtTpekzjrxBUR5gMmTMek3T97E",
+    authDomain: "schoolpro-c3505.firebaseapp.com",
+    databaseURL: "https://schoolpro-c3505.firebaseio.com",
+    projectId: "schoolpro-c3505",
+    storageBucket: "schoolpro-c3505.appspot.com",
+    messagingSenderId: "642665497739",
+    appId: "1:642665497739:web:76facc9178747f5cb8408e"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
